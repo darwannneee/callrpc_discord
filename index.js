a15a4a5bcbf5cdca6e40b95998ceb1d3f468b6a4
darwannneee <darwanchannel1@gmail.com>
2024-03-06 16:19:49 +0000

    added new rpc

--- index.js
+++ index.js
@@ -1,106 +1,192 @@
 import { ethers } from "ethers";
-import {REST, Routes, Client, GatewayIntentBits, SlashCommandBuilder} from "discord.js"
-import fs from "fs"
-import "dotenv/config"
+import { REST, Routes, Client, GatewayIntentBits, SlashCommandBuilder } from "discord.js";
+import fs from "fs";
+import "dotenv/config";
 
-const {DISCORD_ID, DISCORD_TOKEN} = process.env;
+const { DISCORD_ID, DISCORD_TOKEN } = process.env;
 
 // Get Data from JSON
-const getData = JSON.parse(fs.readFileSync('./rpc.json', {encoding: 'utf8'}))
+let getData = [];
+try {
+    getData = JSON.parse(fs.readFileSync('./rpc.json', { encoding: 'utf8' }));
+} catch (error) {
+    console.error("Error reading or parsing rpc.json:", error);
+}
 
 // Initiati Client Discord
-const client = new Client({ intents : [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages]})
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
 
 // Slash Command for Add RPC
 const commands = new SlashCommandBuilder()
     .setName('ping')
     .setDescription('for check api')
-    .addStringOption(option => 
+    .addStringOption(option =>
         option.setName('rpc')
-        .setDescription('input your rpc')
-        .setRequired(true))
+            .setDescription('input your rpc')
+            .setRequired(true));
 
-
-        // Slash Command for Add RPC
+// Slash Command for List RPC
 const listRPC = new SlashCommandBuilder()
-.setName('listrpc')
-.setDescription('for check list RPC')
+    .setName('listrpc')
+    .setDescription('for check list RPC');
+
+    // Slash Command for List RPC
+const totalRPC = new SlashCommandBuilder()
+.setName('totalrpc')
+.setDescription('for check total RPC upload on this BOT');
+
 // Rest for add 
 const rest = new REST({ version: '10' }).setToken(DISCORD_TOKEN);
-  
+
 // Send Rest
 try {
-  await rest.put(Routes.applicationCommands(DISCORD_ID), { body: [commands, listRPC] });
+    await rest.put(Routes.applicationCommands(DISCORD_ID), { body: [commands, listRPC, totalRPC] });
 } catch (error) {
-  console.error(error);
+    console.error("Error sending REST command:", error);
 }
 
 // Jika Client Menyala (Online)
 client.on('ready', () => {
-  try {
-    console.log("Bot Online (+)");
-  } catch(error) {
-    console.error(error);
-  }
+    try {
+        console.log("Bot Online (+)");
+    } catch (error) {
+        console.error("Error during 'ready' event:", error);
+    }
 });
-
 
 // Ketika ada Message baru
 client.on('messageCreate', async message => {
-  if(message.content === 'ping') {
-      message.reply('Pong!')
-  }
-})
+    try {
+        if (message.content === 'ping') {
+            message.reply('Pong!');
+        }
+    } catch (error) {
+        console.error("Error during 'messageCreate' event:", error);
+    }
+});
 
 // Ketika ada Interaction Baru
 client.on('interactionCreate', async interaction => {
-  if(interaction.commandName === 'ping') {
-    const getInputRPC = interaction.options.getString('rpc');
-    const getAPIDetails = getData.find(api => api.rpc == getInputRPC);
-    if(getAPIDetails) {
-      interaction.reply('Udah Pernah');
-    } else {
-      const pushToAPI = {
-        rpc: getInputRPC,
-        rpcID: `${getData.length + 1}`,
-        username: interaction.user.username,
-        getCount: 0
-      };
-  
-      getData.push(pushToAPI); // Tambahkan data ke dalam array getData
-      fs.writeFileSync("rpc.json", JSON.stringify(getData), { flag: 'a', encoding: 'utf-8' });
-      interaction.reply('Berhasil Menambahkan');
-    }
-  }  
+    try {
+        if (interaction.commandName === 'ping') {
+            const getInputRPC = interaction.options.getString('rpc');
+            const getAPIDetails = getData.find(api => api.rpc == getInputRPC);
+            if (getAPIDetails) {
+                interaction.reply('Udah Pernah');
+            } else {
+                const pushToAPI = {
+                    rpc: getInputRPC,
+                    rpcID: `${getData.length + 1}`,
+                    username: interaction.user.username,
+                    getCount: 0
+                };
 
-  if(interaction.commandName === 'listrpc') {
-      const getRPCbyUsername = getData.filter(data => data.username == interaction.user.username)
-      const response = JSON.stringify(getRPCbyUsername, null, 2);
-      interaction.reply({content : response, ephemeral: true})
-  }
-})
+                getData.push(pushToAPI); // Tambahkan data ke dalam array getData
+                fs.writeFileSync("rpc.json", JSON.stringify(getData));
+                interaction.reply('Berhasil Menambahkan');
+            }
+        }
 
-// main for Execute RPC
-async function main() {
-    let totalGetCount = 0; // Menghitung total getCount untuk semua RPC
-
-    // Loop melalui setiap entri data RPC
-    getData.forEach(async (entry) => {
-        const provider = new ethers.providers.JsonRpcProvider(entry.rpc);
-        const balance = await provider.getBalance('vitalik.eth');
+        if (interaction.commandName === 'listrpc') {
+            const getRPCbyUsername = getData.filter(data => data.username == interaction.user.username);
+            let response = '';
+            getRPCbyUsername.forEach((rpcData, index) => {
+                response += `${index + 1}. RPC ${index + 1}\n`;
+                response += `   - RPC: ${rpcData.rpc}\n`;
+                response += `   - RpcID: ${rpcData.rpcID}\n`;
+                response += `   - Username: ${rpcData.username}\n`;
+                response += `   - GetCount: ${rpcData.getCount}\n\n`;
+            });
+            
+            response += `Total RPC uploaded by ${interaction.user.username} = ${getRPCbyUsername.length}`    
+            // Maksimum karakter yang diizinkan dalam satu pesan Discord
+            const maxCharacterLength = 1999;
         
-        // Menambahkan getCount untuk entri saat ini
-        entry.getCount++;
-        totalGetCount += entry.getCount;
-        console.log(`Result For ${entry.rpcID} = ${ethers.utils.formatEther(balance)} ETH`)
-        console.log(`getCount for ${entry.rpcID}: ${entry.getCount}`);
-    });
+            // Jika panjang pesan melebihi batas maksimum, potong pesan menjadi potongan yang lebih kecil
+            if (response.length > maxCharacterLength) {
+                const messages = [];
+                let currentMessage = '';
+        
+                // Bagi pesan menjadi beberapa bagian
+                let lines = response.split('\n');
+                for (let i = 0; i < lines.length; i++) {
+                    const line = lines[i];
+        
+                    // Jika penambahan baris berikutnya melebihi batas karakter, tambahkan pesan yang ada ke daftar pesan
+                    if ((currentMessage + line).length > maxCharacterLength) {
+                        messages.push(currentMessage);
+                        currentMessage = '';
+                    }
+                    
+                    currentMessage += line + '\n';
+                }
+        
+                // Pastikan pesan terakhir ditambahkan
+                if (currentMessage.length > 0) {
+                    messages.push(currentMessage);
+                }
+        
+                // Kirim pesan-pesan yang telah dibagi
+                interaction.reply({ content: messages.shift(), ephemeral: true }).then(() => {
+                    messages.forEach((msg, index) => {
+                        setTimeout(() => {
+                            interaction.followUp({ content: msg, ephemeral: true });
+                        }, (index + 1) * 500); // Menambahkan penundaan untuk menghindari error InteractionNotReplied
+                    });
+                }).catch(error => {
+                    console.error('Error replying to interaction:', error);
+                });
+            } else {
+                // Jika panjang pesan kurang dari batas maksimum, kirim pesan langsung
+                interaction.reply({ content: response, ephemeral: true }).catch(error => {
+                    console.error('Error replying to interaction:', error);
+                });
+            }
+        }        
+        
 
-    // Menulis ulang data yang telah diperbarui ke file rpc.json
-    fs.writeFileSync('./rpc.json', JSON.stringify(getData, null, 2), { encoding: 'utf8' });
+        if (interaction.commandName === 'totalrpc') {
+            interaction.reply(`Total semua RPC yang di upload = ${getData.length} RPC`);
+        }
+    } catch (error) {
+        console.error("Error during 'interactionCreate' event:", error);
+    }
+});
+
+// Main function for Execute RPC
+async function main() {
+    try {
+        let totalGetCount = 0; // Menghitung total getCount untuk semua RPC
+
+        // Loop through each RPC data entry
+        for (const entry of getData) {
+            try {
+                const provider = new ethers.providers.JsonRpcProvider(entry.rpc);
+                const balance = await provider.getBalance('0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045');
+
+                // Menambahkan getCount untuk entri saat ini
+                entry.getCount++;
+                totalGetCount += entry.getCount;
+                console.log(`Result For ${entry.rpcID} = ${ethers.utils.formatEther(balance)} ETH`);
+                console.log(`getCount for ${entry.rpcID}: ${entry.getCount}`);
+            } catch (rpcError) {
+                console.error(`Error processing RPC ${entry.rpcID}:`, rpcError);
+            }
+        }
+
+        // Menulis ulang data yang telah diperbarui ke file rpc.json
+        fs.writeFileSync('./rpc.json', JSON.stringify(getData, null, 2), { encoding: 'utf8' });
+    } catch (error) {
+        console.error("Error during 'main' function execution:", error);
+    }
 }
 
 // SetInterval agar Main selalu di jalankan
-setInterval(main, 30000); // Menjalankan main setiap 30 detik
+setInterval(main, 60000); // Menjalankan main setiap 30 detik
 
-client.login(DISCORD_TOKEN)
+// Login to Discord
+try {
+    client.login(DISCORD_TOKEN);
+} catch (error) {
+    console.error("Error during Discord login:", error);
+}
